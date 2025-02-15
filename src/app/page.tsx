4bd3991791602443057c26671871cd5c67bfda53
Nicholas Lee <niick0816@gmail.com>
2025-02-15 18:05:35 +0800

    Enhanced layout with responsive design and window height adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import NgauCalculator from "@/components/NgauCalculator";
 
 export default function Home() {
+	const [windowHeight, setWindowHeight] = useState(0);
+
+	useEffect(() => {
+		// Function to update the window height
+		const updateHeight = () => {
+			setWindowHeight(window.innerHeight); // Get the viewport height excluding the nav bar
+		};
+
+		// Set initial height
+		updateHeight();
+
+		// Add resize event listener to update height on window resize
+		window.addEventListener("resize", updateHeight);
+
+		// Cleanup event listener on component unmount
+		return () => {
+			window.removeEventListener("resize", updateHeight);
+		};
+	}, []);
+
 	return (
-		<div className="flex items-top md:items-center justify-center min-h-screen bg-white md:bg-gray-200">
+		<div
+			className="flex flex-col items-center justify-center bg-white md:bg-gray-300"
+			style={{ height: windowHeight }}
+		>
 			<NgauCalculator />
 		</div>
 	);
--- src/components/NgauCalculator.tsx
+++ src/components/NgauCalculator.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { computeBestCombination } from "@/lib/calculator";
@@ -66,38 +67,55 @@ const NgauCalculator = () => {
 	}, [hand, handleCalculate]);
 
 	return (
-		<Card className="w-full max-w-md h-full p-4 md:p-8 bg-white shadow-none border-0 md:border-2 md:border-border md:shadow-light">
-			<div className="text-center text-lg font-semibold mb-4">
-				<Card className={`shadow-none p-5 ${
-									result?.error
-										? "bg-red-500"
-										: result && hand.length === 5
-										? Number(result.points)
-											? Number(result.points) >= 1 && Number(result.points) <= 10
-												? "bg-emerald-300"
-												: "bg-white"
-											: result.hand_type === "Pair"
-											? "bg-cyan-300"
-											: result.hand_type === "Ngau Tongku"
-											? "bg-yellow-300"
-											: "bg-red-300"
-										: "bg-white"
-								}`}>
-									{result && hand.length === 5
-										? Number(result.points)
-											? result.points + " Points"
-											: result.hand_type
-										: "Ngau Calculator"}
-								</Card>
+		<Card className="flex flex-col items-center justify-center flex-grow max-w-md md:max-h-[90vh] gap-10 p-4 md:p-8 bg-white shadow-none border-0 md:border-2 md:border-border md:shadow-light">
+			<div className="flex w-full text-center text-lg font-semibold">
+				<Card
+					className={`w-full shadow-none ${
+						result?.error
+							? "bg-red-500"
+							: result && hand.length === 5
+							? Number(result.points)
+								? Number(result.points) >= 1 &&
+								  Number(result.points) <= 10
+									? "bg-emerald-300"
+									: "bg-white"
+								: result.hand_type === "Pair"
+								? "bg-cyan-300"
+								: result.hand_type === "Ngau Tongku"
+								? "bg-yellow-300"
+								: "bg-red-300"
+							: "bg-white"
+					}`}
+				>
+					<div className="p-5">
+						{result && hand.length === 5 ? (
+							Number(result.points) ? (
+								result.points + " Points"
+							) : (
+								result.hand_type
+							)
+						) : (
+							<div className="flex justify-center items-center gap-2">
+								<Image
+									src="/icon.svg"
+									alt="Ngau Calculator Icon"
+									width={28}
+									height={28}
+								/>
+								<span>Ngau Calculator</span>
+							</div>
+						)}
+					</div>
+				</Card>
 			</div>
-			<div className="flex flex-col gap-5 my-8 md:my-10">
+			<div className="flex flex-col w-full justify-center gap-10 flex-grow md:flex-grow-0">
 				<div className="flex justify-evenly">
 					{[...Array(2)].map((_, index) => (
 						<Card
 							key={index}
 							className="w-24 h-36 flex items-center justify-center bg-white"
 						>
-							<span className="text-xl font-bold">
+							<span className="text-4xl font-bold">
 								{result &&
 								hand.length === 5 &&
 								!result.error &&
@@ -116,13 +134,13 @@ const NgauCalculator = () => {
 						</Card>
 					))}
 				</div>
-				<div className="flex justify-evenly">
+				<div className="flex justify-between">
 					{[...Array(3)].map((_, index) => (
 						<Card
 							key={index + 2}
 							className="w-24 h-36 flex items-center justify-center bg-white"
 						>
-							<span className="text-xl font-bold">
+							<span className="text-4xl font-bold">
 								{result &&
 								hand.length === 5 &&
 								!result.error &&
@@ -142,52 +160,55 @@ const NgauCalculator = () => {
 					))}
 				</div>
 			</div>
-			<div className="grid grid-cols-4 gap-2">
-				{[
-					"A",
-					"1",
-					"2",
-					"3",
-					"J",
-					"4",
-					"5",
-					"6",
-					"Q",
-					"7",
-					"8",
-					"9",
-					"K",
-					"10",
-				].map((card) => (
+			<div>
+				<div className="grid grid-cols-4 gap-2">
+					{[
+						"A",
+						"1",
+						"2",
+						"3",
+						"J",
+						"4",
+						"5",
+						"6",
+						"Q",
+						"7",
+						"8",
+						"9",
+						"K",
+						"10",
+					].map((card) => (
+						<Button
+							key={card}
+							onClick={() => handleCardClick(card)}
+							className="p-8"
+							disabled={
+								hand.length === 5 ||
+								(card === "A" && hand.includes("A")) ||
+								(card !== "A" &&
+									hand.filter((c) => c === card).length >= 4)
+							}
+						>
+							<span className="text-xl [&_svg]:size-6">
+								{card === "A" ? <Spade /> : card}
+							</span>
+						</Button>
+					))}
 					<Button
-						key={card}
-						onClick={() => handleCardClick(card)}
-						className="p-8"
-						disabled={
-							hand.length === 5 ||
-							(card === "A" && hand.includes("A")) ||
-							(card !== "A" && hand.filter((c) => c === card).length >= 4)
-						}
+						variant="neutral"
+						onClick={handleClear}
+						className="p-8 [&_svg]:size-6"
 					>
-						<span className="text-xl [&_svg]:size-6">
-							{card === "A" ? <Spade /> : card}
-						</span>
+						<X />
 					</Button>
-				))}
-				<Button
-					variant="neutral"
-					onClick={handleClear}
-					className="p-8 [&_svg]:size-6"
-				>
-					<X />
-				</Button>
-				<Button
-					variant="neutral"
-					onClick={handleBackspace}
-					className="p-8 [&_svg]:size-6"
-				>
-					<Delete />
-				</Button>
+					<Button
+						variant="neutral"
+						onClick={handleBackspace}
+						className="p-8 [&_svg]:size-6"
+					>
+						<Delete />
+					</Button>
+				</div>
 			</div>
 		</Card>
 	);
